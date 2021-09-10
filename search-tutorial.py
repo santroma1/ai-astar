fce1a5bb98282d6b9674336ff2aa56033d8ceb71
Santiago Roman <santroma1@gmail.com>
2021-09-10 21:34:31 +0200

    update

--- search-tutorial.py
+++ search-tutorial.py
@@ -37,30 +37,63 @@ class Map:
                           [4,5,6,7,8,9],
                           [5,6,7,8,9,10]]
 
+        
 
 
 
-#map is 2 dimensional
-def print_map(map, height, width):
-    if type(map[0]) is not type([0,0]):
+
+#grid is 2 dimensional
+def print_map(grid):
+    if type(grid[0]) is not type([0,0]):
         print("Not a bidimensional array")
         return
-    if not (type(map[0][0]) is type(" ") or type(map[0][0] is type(0))):
+    if not (type(grid[0][0]) is type(" ") or type(grid[0][0] is type(0))):
         print("Not a bidimensional array")
         return
-    for x in range(height):
-        for y in range(width):
-            print(map[x][y], end=" ")
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            print(grid[x][y], end=" ")
         print("")
     return
 
 
 def search_a_star(map):
-    pass
+    visited = [[False for j in range(map.width)] for i in range(map.height)]
+    visited[map.begin[0]][map.begin[1]] = True
+    steps = [[-1 for j in range(map.width)] for i in range(map.height)]
+    action = [[-1 for j in range(map.width)] for i in range(map.height)]
+    path = [["-" for j in range(map.width)] for i in range(map.height)]
+
+    x = map.start[0]
+    y = map.start[1]
+    h = map.manhattan[x][y]
+    g = 0
+    f = g + h
+
+    expansion_list = [[f,g,x,y]]
+
+    found = False
+    give_up = False
+    count = 0
+
+    x2 = 0
+    y2 = 0
+
+    return steps, path
+
+    
+
+    
 
 
 map1 = Map()
-print_map(map1.manhattan, map1.height, map1.width)
-print_map(map1.grid, map1.height, map1.width)
+print_map(map1.manhattan)
+print("")
+print_map(map1.grid)
+print("")
 
-#search_a_star(map1)
+
+steps, path = search_a_star(map1)
+print_map(steps)
+print("")
+print_map(path)
